513062597c7a549ffeb47d24e2f9e48f09326f6c
Matthew Dockerty <dockertyml@gmail.com>
2019-08-27 20:29:05 +1200

    Create basic add recipe form

--- webapp/server.go
+++ webapp/server.go
@@ -8,9 +8,8 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.FileServer(http.Dir("/vagrant/webapp/static")).ServeHTTP(w, r);
+		http.FileServer(http.Dir("/vagrant/webapp/static")).ServeHTTP(w, r)
 	})
-
 
 	log.Println("Starting recipe webapp server " + os.Getenv("SERVER_NAME"))
 	http.ListenAndServe(":3000", nil)
