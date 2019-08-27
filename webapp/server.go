07a879a8d4facb3c07bb109bd93b072b8c9b36f8
Matthew Dockerty <dockertyml@gmail.com>
2019-08-27 23:01:00 +1200

    General refactoring & modify webapp server request handler to handle serving each page independently

--- webapp/server.go
+++ webapp/server.go
@@ -6,10 +6,24 @@ import (
 	"os"
 )
 
+// const RESOURCE_FOLDER = "/vagrant/webapp/static"
+const resourceFolder = "static"
+
+func requestHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		http.ServeFile(w, r, "static/index.html")
+	case "/recipes", "/recipes/":
+		http.ServeFile(w, r, "static/recipes.html")
+	case "/add", "/add/":
+		http.ServeFile(w, r, "static/add.html")
+	default:
+		http.Error(w, "404 Not Found", 404)
+	}
+}
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.FileServer(http.Dir("/vagrant/webapp/static")).ServeHTTP(w, r)
-	})
+	http.HandleFunc("/", requestHandler)
 
 	log.Println("Starting recipe webapp server " + os.Getenv("SERVER_NAME"))
 	http.ListenAndServe(":3000", nil)
